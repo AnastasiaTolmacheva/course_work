00a2a0c7cf492d5d1f3f360bfb577b3a042b7690
Anastasia <144587912+AnastasiaTolmacheva@users.noreply.github.com>
2023-11-21 21:17:08 +0500

    Добавлена нормализация данных в иерархической кластеризации

--- anomaly_tree.py
+++ anomaly_tree.py
@@ -3,7 +3,7 @@ import pandas as pd
 from scipy.cluster.hierarchy import linkage, dendrogram
 import matplotlib.pyplot as plt
 from sklearn.impute import SimpleImputer
-
+from sklearn.preprocessing import MinMaxScaler
 
 # Создание и соединение с базой данных
 connection = sqlite3.connect('accounts_db.db')
@@ -35,22 +35,29 @@ data.set_index('user_id', inplace=True)
 
 # Заполнение отсутствующих значений в данных
 imputer = SimpleImputer(strategy='mean')  # Берем среднее значение по столбцу
-data.fillna(data.mean(), inplace=True)
+data_imputed = data.fillna(data.mean())
 
-# Закрываем соединение с базой данных
-connection.close()
+# Нормализация признаков
+scaler = MinMaxScaler()
+data_normalized = scaler.fit_transform(data_imputed)
+
+# Сортировка данных по столбцу username_length
+data_sorted = data.sort_values(by='username_length')
 
 # Вычисление матрицы расстояний
-Z = linkage(data, method='complete', metric='euclidean')
+Z = linkage(data_normalized, method='single', metric='euclidean')
 
 # Создание графика дендрограммы
 plt.figure(figsize=(15, 9))
-plt.title("Дендрограмма иерархической кластеризации с методом complete linkage")
+plt.title("Дендрограмма иерархической кластеризации с методом Single Linkage")
 dendrogram(Z,
            orientation='top',
            distance_sort='descending',
            show_leaf_counts=True,
            labels=data.index)  # Подписываем листья как user_id
 
-plt.savefig('anomaly_dendrogramm_complete_ALL.png')
+plt.savefig('anomaly_dendrogramm_single_ALL.png')
 plt.show()
+
+# Закрываем соединение с базой данных
+connection.close()
